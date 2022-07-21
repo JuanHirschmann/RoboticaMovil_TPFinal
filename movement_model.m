215abb8fd395f1bf4189e0171e2f151075289976
JuanHirschmann <juanhirschmann@gmail.com>
2022-07-21 16:24:01 -0300

    Corregi el modelo de movimiento
    Arregle el modelo de movimiento para que no se rompa cuando le pones velocidades 0

--- movement_model.m
+++ movement_model.m
@@ -22,6 +22,9 @@ function predictParticles=movement_model(particle_filter,prev_particles,varargin
         w_=w+normrnd(0,ALPHA_3*sqrt(abs(v))+ALPHA_4*sqrt(abs(w)));
         s_=0+normrnd(0,ALPHA_5*sqrt(abs(v))+ALPHA_6*sqrt(abs(w)));
         theta=prev_particles(row,3);
+        if w_==0
+            w_=0.00001;
+        end
         predictParticles(row,:)=prev_particles(row,:)+[v_/w_*(sin(theta+w_*time_step)-sin(theta)),...
                                                          v_/w_*(cos(theta)-cos(theta+w_*time_step)),...
                                                          (w_+s_)*time_step];
